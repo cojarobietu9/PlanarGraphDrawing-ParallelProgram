c4c0bccf3c78e0641ffb7be3c825c2761deccbcb
Karolina Wiśniewska <153288137+cojarobietu9@users.noreply.github.com>
2026-09-03 13:47:59 +0200

    Remove unused attributes from Edge class
    Removed unused vertex_a and vertex_b attributes.

--- edge.py
+++ edge.py
@@ -1,10 +1,9 @@
+
 class Edge:
     def __init__(self, name, vertex_a_id, vertex_b_id, weight=1.0):
         self.name = name
         self.vertex_a_id = vertex_a_id
         self.vertex_b_id = vertex_b_id
-        self.vertex_a = None
-        self.vertex_b = None
         self.weight = weight
 
 
